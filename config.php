eba05485bfdc8d806b6723ea5045f43b3b1e8bbb
divel00 <dadeviloper@gmail.com>
2025-06-02 13:51:57 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array(
+    'theme_name' => 'Avo CMS',
+    'theme_title' => 'Avo CMS',
+    'app_name' => 'Xibo',
+    'theme_url' => 'http://xibo.org.uk',
+    'cms_source_url' => 'https://github.com/xibosignage/xibo/',
+    'cms_install_url' => 'http://xibo.org.uk/manual/en/install_windows_client.html',
+    'cms_release_notes_url' => 'http://xibo.org.uk/manual/en/release_notes.html'
+);
+
+$config['view_path'] = PROJECT_ROOT . '/web/theme/custom/customTheme/views';
+
+return $config;
